f6d40a6d09cc7a3146e24f56911d0b8ab815dd26
Suhogruzz <madyson123@yandex.ru>
2023-02-17 15:08:02 +0300

    T3

--- task3/task.js
+++ task3/task.js
@@ -2,7 +2,7 @@ const slides = document.querySelectorAll('.slider__item');
 const nextArrow = document.querySelector('.slider__arrow_next');
 const prevArrow = document.querySelector('.slider__arrow_prev');
 const dots = document.querySelectorAll('.slider__dot');
-const getIndex = (slide) => slide === document.querySelector('.slider__item_active');
+const getIndex = (slide) => slide.classList.contains('slider__item_active')
 
 
 nextArrow.addEventListener('click', () => {
@@ -17,12 +17,16 @@ dots.forEach((dot, dotIndex) => {
 
 function changeSlide(nextSlide) {
     let removeActive =  [...slides].findIndex(getIndex);
-    slides[removeActive].classList.toggle('slider__item_active');
-    dots[removeActive].classList.remove('slider__item_active');
 
-    if (nextSlide >= slides.length) {nextSlide = 0;}
-    if (nextSlide < 0) {nextSlide = slides.length - 1;}
+    slides[removeActive].classList.toggle('slider__item_active');
+    dots[removeActive].classList.remove('slider__dot_active');
+    if (nextSlide >= slides.length) {
+        nextSlide = 0;
+    }
+    if (nextSlide < 0) {
+        nextSlide = slides.length - 1;
+    }
     slides[nextSlide].classList.toggle('slider__item_active');
-    dots[nextSlide].classList.add('slider__item_active');
-};
+    dots[nextSlide].classList.add('slider__dot_active');
+}
 
